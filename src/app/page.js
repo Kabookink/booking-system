439bc02a27ab6094ab3d33e44096a050d47ca850
Syahandhika <dhika.farizi@gmail.com>
2025-09-28 19:07:11 +0700

    feat: Implementasi carousel dan logika form testimoni

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import HeroSection from "../components/HeroSection";
+import Testimoni from "../components/Testimoni";  
 
 export default function Home() {
   return (
     <div className="font-sans min-h-screen">
       <main>
         <HeroSection />
+        <Testimoni />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
